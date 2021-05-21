f96ba59e6df804dab1426a0e8cd01717d290af4b
kyohhh <khnm0322@gmail.com>
2021-05-21 20:34:14 +0900

    カード編集機能

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,7 @@
 class CardController < ApplicationController
+
+  before_action :set_card, only: [:show, :edit, :update]
+
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
@@ -14,12 +17,26 @@ class CardController < ApplicationController
   end
 
   def show
-    @card = Card.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
   end
 
   private
     def card_params
       params.require(:card).permit(:card_title, :memo, :list_id)
+    end
+
+    def set_card
+      @card = Card.find(params[:id])
     end
 
 end
