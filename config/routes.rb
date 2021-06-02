aa3835a02b24760b3927aa16aef2f31189916dec
kyohhh <khnm0322@gmail.com>
2021-06-03 00:32:09 +0900

    ゲストユーザーログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # deviseのユーザー機能
   devise_for :users
 
+  # ゲストユーザー機能
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  # トップページ
   root 'top#index'
 
   resources :list, only: [:new, :create, :edit, :update, :destroy] do
